7d79c19f9c54c5ef1a0019242a1183f943619e3f
unknown <amirxonbahodirov2009@outlook.com>
2024-06-28 15:03:02 +0500

    #2. GET,POST,DELETE,PUT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-const express=require("express")
+const express = require("express")
 
-const app=express()
-PORT=5050
+const app = express()
+app.use(express.json())
+PORT = 5050
 
-app.listen(PORT,()=>console.log(`Listening on PORT:${PORT}`))
+
+app.get('/', (req, res) => {    
+    res.status(200).json({massage:"salom"})
+    res.get("Hello GET")
+})
+
+app.post('/', (req, res) => {
+    const {name, surname}=req.body
+    const massage=`fullname: ${name} ${surname}`
+    console.log(massage);
+    res.send("Hello POST")
+})
+
+app.delete("/:id",(req,res)=>{
+    const {id}=req.params
+    console.log(id);
+    res.send("Hello DELETE")
+})
+
+app.put("/:id",(req,res)=>{
+    const {id}=req.params
+    const body=req.body
+    console.log({id,body});
+    res.send("Hello PUT")
+})
+app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`))
